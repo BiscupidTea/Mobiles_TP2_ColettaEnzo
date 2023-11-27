22f81866d3ba294ddc01367a0e2a4635950facb8
Enzo Coletta <coletta.enzo@yahoo.com.ar>
2023-11-26 23:18:22 -0300

    fix UI problems

---
+++ Assets/Scripts/DebugManager.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DebugManager : MonoBehaviour
+{
+    private const string packName = "com.coletta";
+    private const string className = packName + ".colettalogger";
+    private int currentLogType;
+    private string[] tempArray;
+    [SerializeField] private GameObject textPanel;
+    [SerializeField] private GameObject textObject;
+    [SerializeField] private List<GameObject> createdLogs;
+
+#if UNITY_ANDROID
+
+    private AndroidJavaClass _pluginUnityClass;
+    private AndroidJavaObject _pluginInstance;
+    private AndroidJavaObject _unityActivity;
+    public TextMeshProUGUI _label;
+
+    private void Start()
+    {
+        currentLogType = 0;
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            _pluginUnityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            _unityActivity = _pluginUnityClass.GetStatic<AndroidJavaObject>("currentActivity");
+            _pluginInstance = new AndroidJavaObject(className);
+            if (_pluginInstance == null)
+            {
+                Debug.Log("Plugin Instance is NULL");
+                return;
+            }
+
+            Application.logMessageReceived += Application_logMessageReceived;
+            _pluginInstance.CallStatic("reciveUnityActivity", _unityActivity);
+
+            Debug.Log("Unity Java Class Created");
+        }
+    }
+
+    private void Application_logMessageReceived(string condition, string stacktrace, LogType type)
+    {
+        switch (type)
+        {
+            case LogType.Error:
+                _pluginInstance.Call("SendError", condition);
+                SendToWrite(condition, LogType.Error);
+                break;
+            case LogType.Assert:
+                Debug.Log("AssertLog");
+                break;
+            case LogType.Warning:
+                _pluginInstance.Call("SendWarning", condition);
+                SendToWrite(condition, LogType.Warning);
+                break;
+            case LogType.Log:
+                _pluginInstance.Call("SendLog", condition);
+                SendToWrite(condition, LogType.Log);
+                break;
+            case LogType.Exception:
+                Debug.Log("ExceptionLog");
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(type), type, null);
+        }
+    }
+
+    public void RunPlugin()
+    {
+        Debug.Log("Runing Plugin");
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            _label.text = _pluginInstance.Call<string>("GetLOGTAG");
+        }
+    }
+
+    public void CreateLog(string message)
+    {
+        string data = "";
+        LogType currentLog = LogType.Log;
+        switch (currentLogType)
+        {
+            case 0:
+                Debug.Log("Debug Log - " + message);
+                currentLog = LogType.Log;
+                data = "Debug Log - " + message;
+                break;
+            case 1:
+                Debug.Log("Warning Log - " + message);
+                currentLog = LogType.Warning;
+                data = "Warning Log - " + message;
+                break;
+            case 2:
+                Debug.Log("Error Log - " + message);
+                currentLog = LogType.Error;
+                data = "Error Log - " + message;
+                break;
+        }
+
+        currentLogType++;
+        if (currentLogType >= 3)
+            currentLogType = 0;
+        SendToWrite(data, currentLog);
+    }
+
+    public void SendToWrite(string data, LogType fileType)
+    {
+        _pluginInstance.Call("writeToFile", "Logs.txt", data);
+    }
+
+    public void SendToReadFile()
+    {
+        string temp;
+        temp = _pluginInstance.Call<string>("readFromFile", "Logs.txt");
+        tempArray = temp.Split("\n");
+        for (int i = 0; i < tempArray.Length; i++)
+        {
+            GameObject newText = Instantiate(textObject, textPanel.transform);
+            newText.GetComponent<TextMeshProUGUI>().text = tempArray[i];
+            createdLogs.Add(newText);
+        }
+    }
+
+    public void ShowAlert()
+    {
+        for (int i = 0; i < createdLogs.Count; i++)
+        {
+            Destroy(createdLogs[i].gameObject);
+            createdLogs.Remove(createdLogs[i]);
+        }
+        createdLogs.Clear();
+        Debug.Log("Unity Alert Show");
+        _pluginInstance.Call("ShowAlert");
+    }
+#endif
+}
--- ColettaPlugins/ColettaLogger/src/main/java/com/coletta/colettalogger/Logger.java
+++ ColettaPlugins/ColettaLogger/src/main/java/com/coletta/colettalogger/Logger.java
@@ -3,78 +3,94 @@ package com.coletta.colettalogger;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Logger {
-    static final String LOGTAG = "ColettaLogg";
-    static ColettaLogger _instance = null;
-    private static Activity unityActivity;
-    AlertDialog.Builder builder;
-    public String filename;
-    List<String> warnings = new ArrayList<>();
-    List <String> errors = new ArrayList<>();
-    List <String> debug = new ArrayList<>();
+    private static final String LOGTAG = "ColettaLogg";
+    private static Logger instance = null;
+    private Activity unityActivity;
+    private AlertDialog.Builder builder;
+    private String filename;
+    private List<String> warnings = new ArrayList<>();
+    private List<String> errors = new ArrayList<>();
+    private List<String> debug = new ArrayList<>();
 
-    public static void reciveUnityActivity(Activity uActivity)
-    {
+    // Método para obtener la instancia singleton
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    // Método para recibir la actividad de Unity
+    public void setUnityActivity(Activity uActivity) {
         unityActivity = uActivity;
     }
-    public interface AlertCallback
-    {
-        public void onPositive(String message);
-        public void onNegative(String message);
+
+    // Interfaz de Alerta (Callback)
+    public interface AlertCallback {
+        void onPositive(String message);
+        void onNegative(String message);
     }
 
-    public void CreateAlert(AlertCallback alertCallback)
-    {
-        Log.v(LOGTAG,"Android Create Alert");
+    // Método para escribir en el archivo
+    private void writeToFile(String fileName, List<String> data) {
+        Context context = unityActivity.getApplicationContext();
+        File file = new File(context.getExternalFilesDir(null), fileName);
+
+        try (FileWriter fileWriter = new FileWriter(file, true)) {
+            for (String entry : data) {
+                fileWriter.append(entry).append("\n");
+            }
+        } catch (IOException e) {
+            Log.e(LOGTAG, "Error writing to file: " + e.toString());
+        }
+    }
+
+    // Método para eliminar el archivo de registro
+    public void deleteLogFile() {
+        Context context = unityActivity.getApplicationContext();
+        File logFile = new File(context.getExternalFilesDir(null), filename);
+
+        if (logFile.exists() && logFile.delete()) {
+            Toast.makeText(context, "The file: " + filename + " has been deleted", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "The file: " + filename + " does not exist", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Método para crear una alerta
+    public void createAlert(AlertCallback alertCallback) {
         builder = new AlertDialog.Builder(unityActivity);
         builder.setMessage("Do you want to delete the log file?");
         builder.setCancelable(false);
-        builder.setPositiveButton(
-                "Yes",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Log.v(LOGTAG,"Clicked From Pluggin - YES");
-                        alertCallback.onPositive("Clicked Yes");
-                        dialogInterface.cancel();
-                    }
-                }
-        );
-        builder.setNegativeButton(
-                "No",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Log.v(LOGTAG,"Clicked From Pluggin - NO");
-                        alertCallback.onNegative("Clicked NO");
-                        dialogInterface.cancel();
-                    }
-                }
-        );
+
+        builder.setPositiveButton("Yes", (dialogInterface, i) -> {
+            alertCallback.onPositive("Clicked Yes");
+            deleteLogFile();
+            dialogInterface.cancel();
+        });
+
+        builder.setNegativeButton("No", (dialogInterface, i) -> {
+            alertCallback.onNegative("Clicked NO");
+            dialogInterface.cancel();
+        });
     }
 
-    public void ShowAlert()
-    {
-        Log.v(LOGTAG,"Android Show Alert");
-        AlertDialog alert = builder.create();
-        alert.show();
+    // Método para mostrar la alerta
+    public void showAlert() {
+        if (builder != null) {
+            builder.create().show();
+        } else {
+            Log.e(LOGTAG, "Alert not created. Call createAlert() first.");
+        }
     }
 }
